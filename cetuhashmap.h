d0e181028799328c34e5146aa4900ff473ca2a1d
i.zinger <zingerw@gmail.com>
2024-11-24 17:25:40 +0200

    implementing the functionality in the most basic way using std, just to make myself a bit more happy for now

--- cetuhashmap.h
+++ cetuhashmap.h
@@ -3,6 +3,8 @@
 
 #include <optional>
 
+#include <vector>
+
 template<typename K, typename V>
 class CeTuHashMap
 {
@@ -15,20 +17,18 @@ public:
     // Insert a new pair into the hashmap
     void insert(K key, V value)
     {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys[i] == key) { return; }
+        }
 
+        keys  .push_back(key);
+        values.push_back(value);
     }
 
     // Lookup the given key in the map, if the key is not found return nullptr
     std::optional<V> lookup(K key)
     {
-        return std::nullopt; // implement CeTu::nullopt ?
-
-        //if ( 0 /* ~ key exists */ ) {
-        //    return {};
-        //} else {
-        //    //return nullptr /*std::nullopt_t*/;
-        //    return nullptr;
-        //}
+        return std::nullopt;
     }
 
     // Delete a pair with the key in the hashmap
@@ -39,7 +39,11 @@ public:
 
 private:
 
-    // TODO
+    // ok, we need some custom memory management, as always
+    std::vector<K> keys;
+    std::vector<V> values;
+
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -39,19 +39,6 @@ struct STRC
     int val;
 };
 
-// ========================================
-//template<typename K, typename V>
-//std::optional<V> foo(K key)
-//{
-//    return std::nullopt; // implement CeTu::nullopt ?
-//}
-
-//template<typename T>
-//std::optional<T> foo(T val)
-//{
-//    return std::nullopt; // implement CeTu::nullopt ?
-//}
-
 int main() {
 
 // =====================================================
@@ -70,63 +57,66 @@ int main() {
     CeTuHashMap<int, int> intMap;
     intMap.insert(1, 2);
     auto data = intMap.lookup(1);
+
+    cout << "data: " << ( (void*)(*data) ) << endl;
+
     if (data) {
         cout << "data: " << *data << endl;
     } else {
         cout << "Key not found." << endl;
     }
 
-// =====================================================
-    // Attempt to lookup a key that doesn't exist
-    auto missingData = intMap.lookup(3);
-    if (missingData) {
-        cout << "Missing data: " << *missingData << endl;
-    } else {
-        cout << "Key 3 not found." << endl;
-    }
-
-// =====================================================
-    // Erase a key and then attempt to look it up
-    intMap.erase(1);
-    auto erasedData = intMap.lookup(1);
-    if (erasedData) {
-        cout << "Erased data: " << *erasedData << endl;
-    } else {
-        cout << "Key 1 not found after erase." << endl;
-    }
-
-// =====================================================
-    // Test with std::string as key and double as value
-    CeTuHashMap<string, double> stringMap;
-    stringMap.insert("pi", 3.14159);
-    auto piValue = stringMap.lookup("pi");
-    if (piValue) {
-        cout << "pi: " << *piValue << endl;
-    } else {
-        cout << "Key 'pi' not found." << endl;
-    }
-
-// =====================================================
-    // Insert additional values and demonstrate lookup
-    stringMap.insert("e", 2.71828);
-    auto eValue = stringMap.lookup("e");
-    if (eValue) {
-        cout << "e: " << *eValue << endl;
-    } else {
-        cout << "Key 'e' not found." << endl;
-    }
-
-// =====================================================
-    // Erase a key and then attempt to look it up
-    stringMap.erase("pi");
-    auto erasedPiValue = stringMap.lookup("pi");
-    if (erasedPiValue) {
-        cout << "Erased pi value: " << *erasedPiValue << endl;
-    } else {
-        cout << "Key 'pi' not found after erase." << endl;
-    }
-
-    // TODO : remove garbage
+//// =====================================================
+//    // Attempt to lookup a key that doesn't exist
+//    auto missingData = intMap.lookup(3);
+//    if (missingData) {
+//        cout << "Missing data: " << *missingData << endl;
+//    } else {
+//        cout << "Key 3 not found." << endl;
+//    }
+//
+//// =====================================================
+//    // Erase a key and then attempt to look it up
+//    intMap.erase(1);
+//    auto erasedData = intMap.lookup(1);
+//    if (erasedData) {
+//        cout << "Erased data: " << *erasedData << endl;
+//    } else {
+//        cout << "Key 1 not found after erase." << endl;
+//    }
+//
+//// =====================================================
+//    // Test with std::string as key and double as value
+//    CeTuHashMap<string, double> stringMap;
+//    stringMap.insert("pi", 3.14159);
+//    auto piValue = stringMap.lookup("pi");
+//    if (piValue) {
+//        cout << "pi: " << *piValue << endl;
+//    } else {
+//        cout << "Key 'pi' not found." << endl;
+//    }
+//
+//// =====================================================
+//    // Insert additional values and demonstrate lookup
+//    stringMap.insert("e", 2.71828);
+//    auto eValue = stringMap.lookup("e");
+//    if (eValue) {
+//        cout << "e: " << *eValue << endl;
+//    } else {
+//        cout << "Key 'e' not found." << endl;
+//    }
+//
+//// =====================================================
+//    // Erase a key and then attempt to look it up
+//    stringMap.erase("pi");
+//    auto erasedPiValue = stringMap.lookup("pi");
+//    if (erasedPiValue) {
+//        cout << "Erased pi value: " << *erasedPiValue << endl;
+//    } else {
+//        cout << "Key 'pi' not found after erase." << endl;
+//    }
+//
+//    // TODO : remove garbage
     cin >> inputTemp;
 
     return 0;
